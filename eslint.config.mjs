5335d3e5155291aefb81852f842e7d55f72231f9
racoonamatatas <emacanghaill@gmail.com>
2026-01-13 13:40:22 +0100

    remove redundant comment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,49 +1,39 @@
-import {defineConfig} from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
-import ts from "typescript-eslint";
-import vue from "eslint-plugin-vue";
-import vueParser from "vue-eslint-parser";
+import {defineConfig} from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import vue from 'eslint-plugin-vue';
+import vueParser from 'vue-eslint-parser';
 
 export default defineConfig([
     {
-        files: ["**/*.js", "**/*.ts", "**/*.vue"],
+        files: ['**/*.js', '**/*.ts', '**/*.vue'],
         extends: [js.configs.recommended],
     },
     {
-        files: ["**/*.ts", "**/*.vue"],
+        files: ['**/*.ts', '**/*.vue'],
         extends: [ts.configs.recommended],
     },
     {
-        files: ["**/*.vue"],
-        extends: [vue.configs["flat/essential"]],
+        files: ['**/*.vue'],
+        extends: [vue.configs['flat/essential']],
     },
     {
         languageOptions: {
             ecmaVersion: 2022,
-            sourceType: "module",
+            sourceType: 'module',
             globals: {
                 ...globals.browser,
-                defineEmits: "readonly",
-                defineProps: "readonly",
-                withDefaults: "readonly",
+                defineEmits: 'readonly',
+                defineProps: 'readonly',
+                withDefaults: 'readonly',
             },
             parser: vueParser,
             parserOptions: {
                 parser: ts.parser,
-                extraFileExtensions: [".vue"],
+                extraFileExtensions: ['.vue'],
                 ecmaFeatures: {jsx: true},
             },
         },
     },
 ]);
-
-
-/*
-Navigeer in je terminal naar de map van je Vue-project
-run het volgende commando:
-
-npm i -D eslint-plugin-vue typescript-eslint
-
-Hiermee installeer je ESLint in je Vue-project. 
-*/
